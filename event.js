fd33536a9d48541a09c02c577a53391752011409
Cyancat <aycnight@gmail.com>
2017-07-16 03:51:38 +0800

    Init

---
+++ event.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         HowWTSucks
+// @namespace    https://reimu.worktile.com/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Cyan
+// @match        https://reimu.worktile.com/*
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js
+// ==/UserScript==
+
+// TODO__2: Set a rule to capture the specific URL, replce all element on page with the custumized task detail
+// TODO__2: Get task detail with api https://reimu.worktile.com/api/tasks/no/2210 , then fill the page
+// TODO__2: Get all actions of task ready to work
+
+(function() {
+    'use strict';
+
+    unsafeWindow.document.addEventListener('click', function(e){
+        var time_h = setTimeout(function(){
+          if( $('.card-window').length > 0 ) {
+              time_h = null;
+
+              var d_taskNum = $('.card-sidebar .task-num .num');
+              d_taskNum.replaceWith('<a href="#">' + d_taskNum.text() + '</a>');
+
+              // TODO__1: Give the num text an event to open new window with specific URL
+          }
+        }, 1000);
+    }, false);
+
+})();
